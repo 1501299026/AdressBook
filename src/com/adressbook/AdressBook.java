77fed61d7fc1f967caeeaee4c05d10f5688a474c
1501299026 <sinumsd98@gmail.com>
2021-05-22 22:20:52 -0700

    ADDED: Created contest sucessfuly added

--- src/com/adressbook/AdressBook.java
+++ src/com/adressbook/AdressBook.java
@@ -1,10 +1,47 @@
 package com.adressbook;
-
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 public class AdressBook {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
        System.out.println("Welcome to AdressBook");
+       List<String> PersonDetail = new ArrayList<String>();
+       Scanner input = new Scanner(System.in);
+
+       System.out.print("Enter the First name : ");
+       String fname = input.nextLine();
+
+       System.out.print("Enter the Last name : ");
+       String lname = input.nextLine();
+
+       System.out.print("Enter the Address : ");
+       String address = input.nextLine();
+
+       System.out.print("Enter the City : ");
+       String city = input.nextLine();
+
+       System.out.print("Enter the state : ");
+       String state = input.nextLine();
+
+       System.out.print("Enter the Zip Code : ");
+       String zip = input.nextLine();
+
+       System.out.print("Enter the phone number: ");
+       String phone = input.nextLine();
+       
+       System.out.print("Enter the Email: ");
+       String email = input.nextLine();
+
+       PersonDetail.add(fname);
+       PersonDetail.add(lname);
+       PersonDetail.add(address);
+       PersonDetail.add(city);
+       PersonDetail.add(state);
+       PersonDetail.add(zip);
+       PersonDetail.add(phone);
+       PersonDetail.add(email);
 	}
 
 }
