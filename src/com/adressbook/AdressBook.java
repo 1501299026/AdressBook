1641371dd089f070578a22ebee4287f31791a0e2
1501299026 <sinumsd98@gmail.com>
2021-05-22 22:41:59 -0700

    ADDED: Edit contact details added sucessfuly

--- src/com/adressbook/AdressBook.java
+++ src/com/adressbook/AdressBook.java
@@ -1,51 +1,103 @@
 package com.adressbook;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.ListIterator;
 import java.util.Scanner;
+
 public class AdressBook {
+	public static ArrayList<String> createContact()
+	{
+		ArrayList<String> PersonDetail = new ArrayList<String>(1);
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the First name : ");
+        String fname = input.nextLine();
+
+        System.out.print("Enter the Last name : ");
+        String lname = input.nextLine();
+
+        System.out.print("Enter the Address : ");
+        String address = input.nextLine();
+
+        System.out.print("Enter the City : ");
+        String city = input.nextLine();
+
+        System.out.print("Enter the state : ");
+        String state = input.nextLine();
+
+        System.out.print("Enter the Zip Code : ");
+        String zip = input.nextLine();
+
+        System.out.print("Enter the phone number: ");
+        String phone = input.nextLine();
+        
+        System.out.print("Enter the Email: ");
+        String email = input.nextLine();
+        
+		PersonDetail.add(fname);
+        PersonDetail.add(lname);
+        PersonDetail.add(address);
+        PersonDetail.add(city);
+        PersonDetail.add(state);
+        PersonDetail.add(zip);
+        PersonDetail.add(phone);
+        PersonDetail.add(email);
+
+        for (String str : PersonDetail)
+        {
+            System.out.println(str + " "); 
+        }
+        return PersonDetail;
+	}
+	/**
+	 * created a function to edit the details from the console.
+	 * @param personDetail
+	 */
+	public static void editContact( ArrayList<String> personDetail)
+	{
+		ListIterator<String> iterator = personDetail.listIterator();
+		System.out.println("enter old value");
+		Scanner input = new Scanner(System.in);
+		String originalValue = input.nextLine();
+		System.out.println("enter new value");
+		String value = input.nextLine();
+		while (iterator.hasNext()) {
+		     String next = iterator.next();
+		     if (next.equals(originalValue)) {
+		         //Replace element
+		         iterator.set(value);
+		     } 
+		 }
+		 for (String str : personDetail)
+	        {
+	            System.out.println(str + " "); 
+	        }
+	}
+	/**
+	 * all functions are passes in main function to perform .
+	 * @param args
+	 */
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-       System.out.println("Welcome to AdressBook");
-       List<String> PersonDetail = new ArrayList<String>();
-       Scanner input = new Scanner(System.in);
-
-       System.out.print("Enter the First name : ");
-       String fname = input.nextLine();
-
-       System.out.print("Enter the Last name : ");
-       String lname = input.nextLine();
-
-       System.out.print("Enter the Address : ");
-       String address = input.nextLine();
-
-       System.out.print("Enter the City : ");
-       String city = input.nextLine();
-
-       System.out.print("Enter the state : ");
-       String state = input.nextLine();
-
-       System.out.print("Enter the Zip Code : ");
-       String zip = input.nextLine();
-
-       System.out.print("Enter the phone number: ");
-       String phone = input.nextLine();
-       
-       System.out.print("Enter the Email: ");
-       String email = input.nextLine();
-
-       PersonDetail.add(fname);
-       PersonDetail.add(lname);
-       PersonDetail.add(address);
-       PersonDetail.add(city);
-       PersonDetail.add(state);
-       PersonDetail.add(zip);
-       PersonDetail.add(phone);
-       PersonDetail.add(email);
-
-       for (String str : PersonDetail)
-       {
-           System.out.println(str); 
+		 System.out.println("Welcome in Address Book");
+	        ArrayList<String> personDetail= createContact();
+	        Scanner input = new Scanner(System.in);
+	        System.out.print("Enter the First name : ");
+	        String fname = input.nextLine();
+	        System.out.print("What you want to perform ? Press 1 for Add the details , press 2 for edit the details : ");
+	        int option = input.nextInt();
+	        switch(option)
+	        {
+	        	case 1:
+	        		createContact();
+	        	break;
+	        	case 2:
+	        		editContact(personDetail);
+	        	break;
+	        	default:
+	        		System.out.print("Please enter the valid number : ");	
 	}
 
 }
